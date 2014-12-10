5e8a42c1f539432504f0f712f0917248e1064090
Egil Hjelmeland <egil.hjelmeland@zenitel.com>
2014-12-10 11:40:52 +0100

    added empty socket module project, + polished postbuild copy commands

---
+++ Modules/socket/socket.cs
@@ -0,0 +1,14 @@
+﻿using LuaDLL = Lua511.LuaDLL;
+using lua_State = System.IntPtr;
+
+namespace Lua511.Module
+{
+    public class socket_core
+    {
+        public static int load(lua_State L)
+        {
+            LuaDLL.lua_newtable(L);
+            return 1;
+        }
+    }
+}
